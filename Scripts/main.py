9e5ae5c920b135873e4ee348db85fc9563d260e4
zhipengli <zhipengli1995@gmail.com>
2019-04-15 09:39:34 +0800

    examples for ui

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,32 +1,14 @@
-# Author: Prabhu Ramachandran <prabhu [at] aero.iitb.ac.in>
-# Copyright (c) 2009, Enthought, Inc.
-# License: BSD Style.
+from traits.api import HasTraits, Instance
+from traitsui.api import View, Item
+from tvtk.pyface.scene_model import SceneModel
+from tvtk.pyface.scene_editor import SceneEditor
+from mayavi.core.ui.mayavi_scene import MayaviScene
+from tvtk.pyface.api import Scene
 
-from mayavi import mlab
-from tvtk.tools import visual
-# Create a figure
-f = mlab.figure(size=(500,500))
-# Tell visual to use this as the viewer.
-visual.set_viewer(f)
+class MyModel(HasTraits):
+    scene = Instance(SceneModel, ())
 
-# A silly visualization.
-mlab.test_plot3d()
+    view = View(Item('scene', height=400, show_label=False,
+                    editor=SceneEditor(scene_class=Scene)))
 
-# Even sillier animation.
-b1 = visual.box()
-b2 = visual.box(x=4.)
-b3 = visual.box(x=-4)
-b1.v = 5.0
-
-@mlab.show
-@mlab.animate(delay=250)
-def anim():
-    """Animate the b1 box."""
-    while 1:
-        b1.x = b1.x + b1.v*0.1
-        if b1.x > 2.5 or b1.x < -2.5:
-            b1.v = -b1.v
-        yield
-
-# Run the animation.
-anim()
+MyModel().configure_traits()
